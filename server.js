191d644f8f3d6153bc15db4f6f72c28520224a97
swiftFox <rcwilson995@gmail.com>
2016-08-07 12:22:20 +0100

    Refactioring Validation and Trimming using Underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,15 +13,12 @@ app.get('/', function (req, res) {
     res.json(todos);
 });
 
+
+//GET /todos/:id
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
+    var matchedTodo = _.findWhere(todos, {id: todoId});
 
-    todos.forEach(function (todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
 
     if  (matchedTodo) {
         res.json(matchedTodo);
@@ -30,11 +28,23 @@ app.get('/todos/:id', function(req, res) {
 });
 
 app.post('/todos', function (req, res) {
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
 
+    if (!_.isBoolean(body.completed)  || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
+
+    //add id field
     body.id = todoNextId++;
+
+    //push body into array
     todos.push(body);
+
+
     res.json(body);
+
 });
 
 app.listen(PORT, function () {
