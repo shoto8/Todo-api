e02c82d4113d5ce700270034c70cd2b4d083a3a2
swiftFox <rcwilson995@gmail.com>
2016-08-07 12:46:21 +0100

    Adding Delete

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
+app.get('/todos', function (req, res) {
     res.json(todos);
 });
 
@@ -45,6 +45,20 @@ app.post('/todos', function (req, res) {
 
     res.json(body);
 
+});
+
+// DELETE /todos/:id
+
+app.delete('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    if(!matchedTodo) {
+        res.status(404).json({"error": "no todo found with that id"});
+    } else {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    }
 });
 
 app.listen(PORT, function () {
