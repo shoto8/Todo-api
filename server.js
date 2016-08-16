8a2a792a53a55d5cfff8883c9881dd9f9be84349
swiftFox <rcwilson995@gmail.com>
2016-08-16 20:40:46 +0100

    Added DELETE /users/login

--- server.js
+++ server.js
@@ -150,17 +150,25 @@ app.post('/users', function (req, res) {
 // POST /users/login
 app.post('/users/login', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function (user) {
 		var token = user.generateToken('authentication');
+		userInstance = user;
 
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
+		return db.token.create({
+				token: token
+		});
+
+		// if (token) {
+		// 	res.header('Auth', token).json(user.toPublicJSON());
+		// } else {
+		// 	res.status(401).send();
+		// }
+	}).then(function (tokenInstance) {
+			res.header('Auth', tokenInstance.get('token')),json(userInstance.toPublicJSON());
+	}).catch(function () {
 			res.status(401).send();
-		}
-	}, function () {
-		res.status(401).send();
 	});
 });
 
